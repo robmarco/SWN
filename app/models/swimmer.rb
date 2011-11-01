145ae90896918b9ca678ec27a640bfb868c3c950
Robert Marco <robmarco@gmail.com>
2011-11-01 21:16:14 +0000

    Modificado modelo incluyendo método para acceder a apellidos y nombre

--- app/models/swimmer.rb
+++ app/models/swimmer.rb
@@ -3,4 +3,8 @@ class Swimmer < ActiveRecord::Base
   has_many :competition_results, :dependent => :destroy
   
   validates_presence_of :name, :secname, :born, :licence, :category, :state
+  
+  def attributes_id_name
+    { 'id' => id, 'name' => name+' '+secname }
+  end
 end

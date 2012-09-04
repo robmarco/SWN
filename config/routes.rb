e3c4a5c5db9ed52ef35963c686a0530ad47708d8
Roberto Marco <robmarco@gmail.com>
2012-09-04 20:28:43 +0100

    Added gitignore file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 SWN::Application.routes.draw do
-  resources :accounts
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
 
-  get "accounts/upload"
-  get "dashboard/index", :as => :dashboard
+  resources :accounts, :except => [:new, :show, :index]
 
   resources :trainnings, :path => "entrenamientos"
   resources :incidents, :path => "incidencias"
@@ -10,24 +10,13 @@ SWN::Application.routes.draw do
   resources :competitions, :path => "competiciones"
   resources :swimmers, :path => "nadadores"
 
+  get "dashboard/index", :as => :dashboard
   get "pages/index"
   get "pages/about"
 
   devise_for :users
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  root :to => "pages#index"
 
   # Sample resource route with options:
   #   resources :products do
@@ -62,13 +51,4 @@ SWN::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => "pages#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
